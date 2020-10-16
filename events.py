3053aa3f6b7cda0ed835c78e70947d94f0923d44
Joe Downs <joedowns502@gmail.com>
2020-10-16 01:37:10 -0400

    events.py: add getSeason() to return season given event number
    Since seasons follow a regular pattern, the user does not need to
    specify the season, it can be calculated for them
    + getSeason()

--- events.py
+++ events.py
@@ -1,6 +1,7 @@
 # Any and all functions related to events
 import database.readDB as readDB
 import database.writeDB as writeDB
+import math
 
 # Create event given title and number (e.g., CDL 203), season, and date
 def createEvent(cursor, title, number, seasonID, date):
@@ -9,3 +10,10 @@ def createEvent(cursor, title, number, seasonID, date):
                       number = number,
                       seasonID = seasonID,
                       date = date)
+
+# Get the appropiate season given a event number
+# 1-10 is Season 1, 11-20 is Season 2, and so on...
+def getSeason(eventNumber):
+    season = int(math.floor(eventNumber / 10))
+    return season
+    
