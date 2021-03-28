b729f41f372f0df31bb02d62617ff212ba900a9a
Joe Downs <joedowns502@gmail.com>
2021-03-27 20:33:47 -0400

    events.py: use SQLite Row instances in place of Event class
    The SQLite Row instance is a much more effective wrapper of info
    pertaining to events than the Event class I had created. SQLite
    returns a list of rows from the database. This can easily replicate
    having a list of Event class instances.
    
    - Event [class]
    ~ getEvents()

--- events.py
+++ events.py
@@ -3,18 +3,6 @@ import database.sqlpyte3.readDB as readDB
 import database.sqlpyte3.writeDB as writeDB
 import math
 
-# Event class for storing data about each event. Class instances can be used
-# to return bundled info about events.
-class Event:
-    def __init__(self, sqlID, eventNumber, eventName, date):
-        self.sqlID = sqlID
-        self.eventNumber = eventNumber
-        self.eventName = eventName
-        self.date = date
-    # Converts the info about the event into a nicely formatted string for output
-    def __str__(self):
-        return f"{self.eventName} {self.eventNumber}\nDate: {self.date}\nSQL ID: {self.sqlID}"
-
 # Create event given title and number (e.g., CDL 203), season, and date
 def createEvent(cursor, title, number, seasonID, date):
     writeDB.insertRow(cursor, "events",
@@ -33,17 +21,10 @@ def getSeason(eventNumber):
 # (e.g., the 10 most recent CDL or RPG events) and return a list of Event class
 # objects containing the SQL ID, title and number, and date.
 def getEvents(cursor, eventName, numOfEvents):
-    # List of ALL the event numbers of the given title
-    eventNumbers = readDB.getValue(cursor, "events", "number", "title", eventName, getMultiple = True,
-                                   sortedResults = True, sortBy = "number", descending = True)
-    # List of the Event class objects
-    eventsList = []
-    # Get the other info pertaining to the events, create an Event class object for
-    # the event and add it to the eventsList list.
-    for i in range(0, numOfEvents):
-        sqlID = int(readDB.getValue(cursor, "events", "ROWID", "number", int(eventNumbers[i])))
-        eventNumber = int(eventNumbers[i])
-        eventDate = readDB.getValue(cursor, "events", "date", "number", eventNumber)
-        tempEvent = Event(sqlID, eventNumber, eventName, eventDate)
-        eventsList.append(tempEvent)
-    return eventsList
+    # SQL command to be executed with a placeholder for how many events to
+    # return.
+    sqlCommand = "SELECT * FROM events ORDER BY number DESC LIMIT ?"
+    cursor.execute(sqlCommand, (numOfEvents,))
+    # Return the list of SQLite3 Row instances
+    return cursor.fetchall()
+
