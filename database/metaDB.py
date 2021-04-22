1d7cb5deb9ff71718c8543e7d56152e7b5e8c252
Joe Downs <joedowns502@gmail.com>
2021-04-22 00:20:33 -0400

    metaDB.py: switch to regular SQL; cleanup
    First of all, sqlpyte was replaced with vanilla SQL commands and
    cursor.execute() statements.
    
    Comments were also reformatted to better fill the 80-column width.
    
    createEventGamemodesTable() was renamed to createGamemodesTable()
    for simplicity.
    
    ~ createEventGamemodesTable() -> createGamemodesTable()
    ~ createEventTable()
    ~ createReminderTable()
    ~ createResultsTable()
    ~ createRSVPTable()
    ~ createSeasonTable()
    ~ createUserTable()
    ~ createVehicleTable()

--- database/metaDB.py
+++ database/metaDB.py
@@ -1,64 +1,65 @@
-import sqlpyte3.writeDB as writeDB
+# Shorthand string used in every table to create the sqlID column, which gives
+# each row a unique ID that autoincrements whenever a row is inserted into the
+# table. (To autoincrement it, NULL must be inserted into its column)
+sqlID = "sqlID INTEGER PRIMARY KEY AUTOINCREMENT"
 
-# Creates table linking gamemodes and the event
-# they are used in
-def createEventGamemodesTable(cursor):
-    writeDB.createTable(cursor, "eventGamemodes",
-                        eventID = "INTEGER",
-                        gamemodeID = "INTEGER")
+# Creates table linking gamemode and the event they are used in
+def createGamemodesTable(curs):
+    gamemodesCommand = f"""
+CREATE TABLE gamemodes ({sqlID}, eventID INTEGER, gameModeID INTEGER)
+    """
+    curs.execute(gamemodesCommand)
 
-# Creates table of events containing info about
-# the event (title, time, etc.)
-def createEventTable(cursor):
-    writeDB.createTable(cursor, "events",
-                        title = "TEXT",
-                        number = "INTEGER",
-                        seasonID = "INTEGER",
-                        date = "TEXT")
+# Creates table of events containing info about the event (title, time, etc.)
+def createEventTable(curs):
+    eventCommand = f"""
+CREATE TABLE events
+({sqlID}, title TEXT, number INTEGER, seasonID INTEGER, date TEXT)
+"""
+    curs.execute(eventCommand)
 
-# Creates table of reminders linked to users
-# and events; stores the reminder as an
-# absolute time (not an offset from the event)
-def createReminderTable(cursor):
-    writeDB.createTable(cursor, "reminders",
-                        eventID = "INTEGER",
-                        time = "TEXT",
-                        userID = "INTEGER")
-    
+# Creates table of reminders linked to users and events; stores the reminder as
+# an absolute time (not an offset from the event)
+def createReminderTable(curs):
+    reminderCommand = f"""
+CREATE TABLE reminders ({sqlID}, eventID INTEGER, time TEXT, userID INTEGER)
+"""
+    curs.execute(reminderCommand)
+
 # Creates table of results for each event and user
-def createResultsTable(cursor):
-    writeDB.createTable(cursor, "results",
-                        eventID =  "INTEGER",
-                        gamemodeID = "INTEGER",
-                        userID = "INTEGER",
-                        place = "INTEGER",
-                        groupNumber = "INTEGER")
-    
-# Creates table of users' registgrations for each
-# event containing info about the registration
-# (who registered, which event, which vehicle, etc.)
-def createRSVPTable(cursor):
-    writeDB.createTable(cursor, "rsvp",
-                        eventID = "INTEGER",
-                        userID = "INTEGER",
-                        vehicleID = "INTEGER")
+def createResultsTable(curs):
+    resultsCommand = f"""
+CREATE TABLE results ({sqlID}, eventID INTEGER, gamemodeID INTEGER,
+userID INTEGER, place INTEGER, groupNumber INTEGER)
+"""
+    curs.execute(resultsCommand)
 
-# Creates table of seasons to be referenced
-# in the events table
-def createSeasonTable(cursor):
-    writeDB.createTable(cursor, "seasons",
-                        number = "INTEGER")
+# Creates table of users' registrations for each event containing info about the
+# registration (who registered, which event, which vehicle, etc.)
+def createRSVPTable(curs):
+    rsvpCommand = f"""
+CREATE TABLE rsvp ({sqlID}, eventID INTEGER, userID INTEGER, vehicleID, INTEGER)
+"""
+    curs.execute(rsvpCommand)
 
-# Creates table of users containing general info
-# about the user (Discord ID, username, etc.)
-def createUserTable(cursor):
-    writeDB.createTable(cursor, "users",
-                        discordID = "INTEGER",
-                        username = "TEXT",
-                        status = "TEXT")
+# Creates table of seasons to be referenced in the events table
+def createSeasonTable(curs):
+    seasonCommand = f"""
+CREATE TABLE seasons ({sqlID}, number INTEGER)
+"""
+    curs.execute(seasonCommand)
 
-# Creates table of vehicles to be referenced
-# by the RSVP table
-def createVehicleTable(cursor):
-    writeDB.createTable(cursor, "vehicles",
-                        name = "TEXT")
+# Creates table of users containing general info about the user (Discord ID,
+# username, etc.)
+def createUserTable(curs):
+    userCommand = f"""
+CREATE TABLE users ({sqlID}, discordID INTEGER, username TEXT, status TEXT)
+"""
+    curs.execute(userCommand)
+
+# Creates table of vehicles to be referenced by the RSVP table
+def createVehicleTable(curs):
+    vehicleCommand = f"""
+CREATE TABLE vehicles ({sqlID}, name TEXT)
+"""
+    curs.execute(vehicleCommand)
