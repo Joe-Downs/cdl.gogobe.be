4a9d6b5712fc663f4e18864992e5f1b94fef5da7
Joe Downs <joedowns502@gmail.com>
2021-04-25 00:02:50 -0400

    spreadsheets.py: init with some info about the script

---
+++ spreadsheets.py
@@ -0,0 +1,10 @@
+# Python Modules
+# Third-Party Modules
+# Custom Modules
+
+# Contains functions and scripts for downloading and converting a spreadsheet
+# (namely Google Sheets) into a SQLite3 database. This will (evenetually)
+# include downloading the spreadsheets from Google, parsing them, and adding all
+# the necessary info to the database.
+
+
