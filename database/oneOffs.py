e41653243ca53f74e0929d8d44a3b2ff2ce1a1fc
Joe Downs <joedowns502@gmail.com>
2021-04-26 23:55:18 -0400

    oneOffs.py: init with some info about module

---
+++ database/oneOffs.py
@@ -0,0 +1,9 @@
+# Python Modules
+# Third-Party Modules
+# Custom Modules
+
+# This module will hold functions which (most likely) will only be used a few
+# times in initially creating the database. These functions will populate the
+# database with data from various CSV files.
+
+
