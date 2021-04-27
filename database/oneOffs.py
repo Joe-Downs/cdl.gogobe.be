c8eca10d5ad9354ba0e3781202822158a79c1df5
Joe Downs <joedowns502@gmail.com>
2021-04-27 00:21:18 -0400

    oneOffs.py: add populateUseres()
    This function takes a strictly-formatted users.csv file (created by
    me either by-hand or through another script) and adds the data to the
    database via a given cursor.
    
    + populateUseres()

--- database/oneOffs.py
+++ database/oneOffs.py
@@ -1,4 +1,6 @@
 # Python Modules
+import csv
+import sqlite3
 # Third-Party Modules
 # Custom Modules
 
@@ -6,4 +8,19 @@
 # times in initially creating the database. These functions will populate the
 # database with data from various CSV files.
 
+# ========================== Populate Database Tables ==========================
 
+# populateUsers() populates the users given by users.csv into the users table of
+# the database of the given cursor.
+def populateUsers(curs):
+    with open("users.csv", newline = '') as csvUsers:
+        userReader = csv.DictReader(csvUsers)
+        for row in userReader:
+            userCommand = """INSERT INTO users
+(sqlID, discordID, username, status) VALUES (NULL, ?, ?, ?)
+"""
+            discordID = int(row["discordID"])
+            username = row["username"]
+            status = row["status"]
+            curs.execute(userCommand, (discordID, username, status,))
+    csvUsers.close()
